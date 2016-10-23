dab96bf8badbf8ca528dd4b453fb5c761a5631bb
93joshmusic <0parsj20@solent.ac.uk>
2016-10-23 17:02:58 +0100

    Camera
    Camera is centered on the player and zooms out once the player reaches a
    certain size.

---
+++ MobileGamesProject2/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    public GameObject player;
+
+    public Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //camera zooms out once the player is a certain size
+
+        if ((player.transform.localScale.magnitude >= 5)&&(player.transform.localScale.magnitude <= 10))
+        {
+            mainCamera.orthographicSize = 10;
+        }
+        else if (player.transform.localScale.magnitude >= 10)
+        {
+            mainCamera.orthographicSize = 20;
+        }
+        else
+        {
+            mainCamera.orthographicSize = 5;
+        }
+	
+	}
+}
--- MobileGamesProject2/Assets/Scripts/PlayerScript.cs
+++ MobileGamesProject2/Assets/Scripts/PlayerScript.cs
@@ -29,7 +29,7 @@ public class PlayerScript : MonoBehaviour {
 	void Update () {
 
 
-
+       
         
 
         //up down left right
